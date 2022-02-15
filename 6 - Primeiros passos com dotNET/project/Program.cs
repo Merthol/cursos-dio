7b1a88bd8aed36cc128d9ae920e2178b5bba252b
Merthol <guilhermer_l@hotmail.com>
2022-02-15 11:40:50 -0300

    Criação de novo projeto para o curso Primeiros passos .NET e atualização de arquivos de execução

---
+++ 6 - Primeiros passos com dotNET/project/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numeroDeVezes = 5;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Bem vindo ao curso da Dio {i + 1}");
+}

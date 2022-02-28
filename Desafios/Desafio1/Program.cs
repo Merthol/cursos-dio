c00da9fac4e1b276c14996ac3a2fb1948942e802
Merthol <guilhermer_l@hotmail.com>
2022-02-28 11:12:15 -0300

    Inclusão dos arquivos do curso Introdução ao HTTP/HTTPS, assim como o certificado do curso, e atualização de outros arquivos anteriores.

---
+++ Desafios/Desafio1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma;
+            int x = int.Parse(Console.ReadLine());
+
+            while (x != 0)
+            {
+                soma = 0;
+                if ((x % 2) == 1)
+                {
+                    x++;
+                }
+
+                for (int y = 0; y < 5; y++)
+                {
+                    soma = soma + x;
+                    x = x + 2;
+                }
+
+                Console.WriteLine(soma);
+                x = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
